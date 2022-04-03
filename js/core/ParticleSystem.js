43e1c0e0877814f40167889f119cfb2b848283f2
MrTowers <dawmc52@gmail.com>
2022-04-03 08:05:50 +0200

    x

--- js/core/ParticleSystem.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "./Component.js";
-export class ParticleSystem extends Component {
-    constructor() {
-        super();
-        this.tag = "particle system";
-    }
-    update() {
-    }
-}
