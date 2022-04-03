02d32a9309d29070829b71306ca8dfd608c935c3
MrTowers <dawmc52@gmail.com>
2022-04-03 20:49:30 +0200

    x

--- Timeoid.js
+++ Timeoid.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron");
 function createWindow () {
     const win = new BrowserWindow({
         fullscreen: true,
-        icon: "assets/textures/icon.png",
+        icon: "icon.ico",
         title: "Timeoid",
         frame: false
     });
